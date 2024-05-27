13f5965611f6c2b3a085adb98fc741e40441ec14
gupettine <gupettine@gmail.com>
2024-05-27 12:00:11 -0300

    refactor: convert to context

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,43 @@
+import { useFormContext } from 'react-hook-form'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
+
+import { FormContainer, TaskInput, MinutesAmountInput } from './styles'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Im going to work in</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Name your project"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Project 1"></option>
+        <option value="Project 2"></option>
+        <option value="Project 3"></option>
+      </datalist>
+
+      <label htmlFor="minutesAmount">for</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutes.</span>
+    </FormContainer>
+  )
+}
